b669f908bc521fa029945a903ecfc584b09e1701
Yenthe666 <yenthevg@gmail.com>
2022-11-07 09:27:20 +0100

    [IMP][934] auto_invoice_advance: better field position & version bump

--- auto_invoice_advance/__manifest__.py
+++ auto_invoice_advance/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '16.0.0.0.1',
+    'version': '16.0.0.1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['sale'],
