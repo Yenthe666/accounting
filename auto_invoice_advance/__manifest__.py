c54fda7e838f2b8c417d23ffc808cfaacb0ff245
Yenthe Van Ginneken <yenthevg@gmail.com>
2022-11-17 13:56:20 +0100

    [IMP] auto_invoice_advance: add app license

--- auto_invoice_advance/__manifest__.py
+++ auto_invoice_advance/__manifest__.py
@@ -18,6 +18,7 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '16.0.0.1.1',
+    'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['sale'],
